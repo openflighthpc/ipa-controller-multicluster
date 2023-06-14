2730d497a4ed5278911837b8abc506c2aa3ef71d
Stu Franks <stu@stuts.uk>
2023-06-14 16:10:47 +0100

    Fix ipa-add-host script

--- openflight-ipa-controller/run_env/openflight-ipa-controller/roles/ipa/templates/ipa-add-host.sh
+++ openflight-ipa-controller/run_env/openflight-ipa-controller/roles/ipa/templates/ipa-add-host.sh
@@ -6,7 +6,7 @@ CLUSTER="$3"
 DOMAIN="{{ site_name }}.{{ domain }}"
 INSECURE_PASSOWRD="{{ insecure_admin_password }}"
 
-if [ -z ${NODE} ] || [ -z ${NODE_IP} ] || [ -z ${CLUSTER } ] ; then
+if [ -z ${NODE} ] || [ -z ${NODE_IP} ] || [ -z ${CLUSTER} ] ; then
     echo "Please provide node, node IP and cluster name"
     echo "  ipa-add-host node01 10.100.1.101 mycluster1"
     exit 1
@@ -14,10 +14,12 @@ fi
 
 echo "{{ secure_admin_password }}" | kinit admin
 
-ipa host-add $NAME.$CLUSTER.$DOMAIN --password="$INSECURE_PASSWORD" --a-ip-address="$NODE_IP"
-ipa hostgroup-add-member $CLUSTER --hosts=$NAME.$CLUSTER.$DOMAIN
+ipa host-add $NODE.$CLUSTER.$DOMAIN --password="$INSECURE_PASSWORD" --ip-address="$NODE_IP"
+ipa hostgroup-add-member $CLUSTER --hosts=$NODE.$CLUSTER.$DOMAIN
 
 echo
-echo "==== TEST ACCESS ===="
+echo "==== NEXT STEPS ===="
+echo "The host will need to be connected to IPA with:"
+echo "  ipa-client-install --no-ntp --mkhomedir --ssh-trust-dns --force-join --realm='{{ realm }}' --server='{{ controller_fqdn }}' -w '{{ insecure_admin_password }}' --domain='${CLUSTER}.$DOMAIN' --unattended --hostname='$NODE.$CLUSTER.$DOMAIN'"
 echo "Initial testing of access to the cluster can be done with:"
 echo "  ipa hbactest --user=${CLUSTER}-testuser --host=$NODE.$CLUSTER.$DOMAIN --service=sshd"
